843bd484171fc5e117904a70dcc3bdb917aadc2c
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-01-28 00:32:09 +0530

    d

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app=express()
 app.use(express.static('./public'))
 
 app.route('/').get((req,res)=>{
-    return res.json({
+    return res.status(200).json({
         "status":"success"
     })
 })
