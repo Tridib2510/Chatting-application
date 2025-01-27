30be7640a51787185744693dd38960b103ec2c45
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-01-28 01:43:37 +0530

    project

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.static('./public'))
 
 
 app.route('/').get((req,res)=>{
- return res.status(200).sendFile('https://chatting-application-n1xs.vercel.app/')
+ return res.status(200).json({
+    "status":"success"
+ })
 })
 
 
